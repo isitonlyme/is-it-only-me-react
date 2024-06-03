78fdcf1ca99200210d48155e7e558d08d1255652
millalinden <milla.linden@hyperisland.se>
2024-06-03 14:53:12 +0200

    marquee

--- src/components/LPSection2.jsx
+++ src/components/LPSection2.jsx
@@ -12,6 +12,7 @@ export default function LPSection2() {
     useRef(null),
     useRef(null),
     useRef(null),
+    useRef(null),
   ];
   const textRef = useRef(null);
 
@@ -43,7 +44,7 @@ export default function LPSection2() {
     if (marqueeRefs.every((ref) => ref.current)) {
       marqueeRefs.forEach((ref, index) => {
         const element = ref.current;
-        const direction = index % 2 === 0 ? 100 : -100;
+        const direction = index % 2 === 0 ? 100 : -200;
 
         gsap.fromTo(
           element,
@@ -56,7 +57,7 @@ export default function LPSection2() {
               trigger: element,
               start: "top bottom",
               end: "bottom top",
-              scrub: true,
+              scrub: index === 0 ? 1 : index === 3 ? 3 : 0.2, // Adjust the scrub value for each span
             },
           }
         );
@@ -80,41 +81,48 @@ export default function LPSection2() {
             id="marqueeDiv"
             className="pt-32 relative w-full h-full overflow-hidden"
           >
-            <div className="flex flex-col pb-20">
+            <div className=" pb-20">
               <span
                 ref={marqueeRefs[0]}
-                className="marquee text-3xl bg-white text-center p-1 mb-5 inline-block transform rotate-2 whitespace-nowrap"
+                className="marquee text-3xl bg-white text-center p-1 mb-5 flex flex-col  mx-52 inline-block transform rotate-2 whitespace-nowrap"
                 style={{ top: "0px" }}
               >
                 that has an official date outfit?
               </span>
               <span
                 ref={marqueeRefs[1]}
-                className="marquee text-3xl  text-center bg-white p-1 mb-5 inline-block transform -rotate-12 whitespace-nowrap"
+                className="marquee text-4xl  text-center bg-white p-1 mb-5 mx-52 inline-block transform -rotate-12 whitespace-nowrap"
                 style={{ top: "30px" }}
               >
                 that has a crush on Bill Clinton?
               </span>
               <span
                 ref={marqueeRefs[2]}
-                className="marquee text-3xl  text-center bg-white p-1 mb-5 inline-block transform rotate-12 whitespace-nowrap"
+                className="marquee text-3xl  text-center bg-white p-1 mb-5 inline-block transform rotate-8 whitespace-nowrap"
                 style={{ top: "60px" }}
               >
                 who feels a bit of relief when plans get canceled?
               </span>
               <span
                 ref={marqueeRefs[3]}
-                className="marquee  text-center text-3xl bg-white p-1 mb-6 inline-block transform -rotate-0 whitespace-nowrap"
+                className="marquee  text-center text-5xl bg-white p-1 mx-32 mb-6 inline-block transform -rotate-0 whitespace-nowrap"
                 style={{ top: "90px" }}
               >
-                who enjoys the smell of gasoline?
+                am I weird?{" "}
               </span>
               <span
                 ref={marqueeRefs[4]}
-                className="marquee text-3xl  text-center bg-white p-1 mb-5 inline-block transform rotate-12 whitespace-nowrap"
+                className="marquee text-3xl text-center bg-white p-1 mb-5 inline-block transform -rotate-0"
                 style={{ top: "90px" }}
               >
                 who judges people by their taste in music or TV shows?
+              </span>
+              <span
+                ref={marqueeRefs[5]}
+                className="marquee text-3xl text-center mx-60 bg-white p-1 mb-5 inline-block transform -rotate-12 whitespace-nowrap"
+                style={{ top: "100px" }}
+              >
+                who enjoys the smell of gasoline?
               </span>
             </div>
           </div>
